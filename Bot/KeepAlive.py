27d836fe1771cadc5e0fa355baf392a29876fad3
Nguyen Quang Huy <78330758+Dev789-del@users.noreply.github.com>
2022-04-04 13:20:19 +0700

    Create KeepAlive.py

---
+++ Bot/KeepAlive.py
@@ -0,0 +1,16 @@
+#This is a small code to keep your bot in discord online 24/7
+from flask import Flask
+from threading import Thread
+
+app = Flask('')
+
+@app.route('/')
+def home():
+    return "Hello. I am alive!"
+
+def run():
+  app.run(host='0.0.0.0',port=8080)
+
+def keep_alive():
+    t = Thread(target=run)
+    t.start()
